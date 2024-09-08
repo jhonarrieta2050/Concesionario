5aeef68d1ed2cd89d39664bf70b858556c89993e
jhon <jhonarrieta2050@gmail.com>
2024-09-08 16:05:39 -0500

    adelantando

---
+++ src/main/java/org/unicolombo/concesionario/clases/compras/factura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.unicolombo.concesionario.clases.compras;
+
+import java.time.LocalDateTime;
+import org.unicolombo.concesionario.clases.usuario.Usuario;
+import org.unicolombo.concesionario.clases.vehiculos.Vehiculos;
+
+public class factura {
+    private Usuario usuario;
+    private Vehiculos vehiculo;
+    private LocalDateTime fecha;
+    private Usuario vendedor;
+}
